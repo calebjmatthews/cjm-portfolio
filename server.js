6ee8a28d8e3c7c9117cef18a98f32ef3c0c9d14d
Caleb Matthews <calebjmatthews@gmail.com>
2016-10-26 21:43:40 -0500

    Transpile changes to js

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-// import express = require('express');
-// import path = require('path');
-// var port: number = process.env.PORT || 8000;
-// var app = express();
-// app.use('/app', express.static(path.resolve(__dirname, 'app')));
-// // var renderIndex = (req: express.Request, res: express.Response) => {
-// //     res.sendFile(path.resolve(__dirname, 'index.html'));
-// // }
-// // 
-// // app.get('/*', renderIndex);
-// var serveStatic = require('serve-static');
-// app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
-// var server = app.listen(port, function() {
-//   var host = server.address().address;
-//   var port = server.address().port;
-//   console.log('This express app is listening on port:' + port);
-// });
-var express = require('express');
-var app = express();
-console.log(app);
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+"use strict";
+const express = require('express');
+const path = require('path');
 var port = process.env.PORT || 8000;
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port);
+var app = express();
+app.use('/app', express.static(path.resolve(__dirname, 'app')));
+// var renderIndex = (req: express.Request, res: express.Response) => {
+//     res.sendFile(path.resolve(__dirname, 'index.html'));
+// }
+// 
+// app.get('/*', renderIndex);
+var serveStatic = require('serve-static');
+app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('This express app is listening on port:' + port);
 });
+// var express = require('express');
+// var app = express();
+// var port: number = process.env.PORT || 8000
+// console.log(app);
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+// app.listen(port, function () {
+//   console.log('Example app listening on port ' + port );
+// }); 
 //# sourceMappingURL=server.js.map
