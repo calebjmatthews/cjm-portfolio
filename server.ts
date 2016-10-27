54200874bf7b8c2c5154c82e8dbf327bb178bc86
Caleb Matthews <calebjmatthews@gmail.com>
2016-10-26 20:48:56 -0500

    Remove leading period from Procfile

--- server.ts
+++ server.ts
@@ -5,17 +5,17 @@ var app = express();
 
 app.use('/app', express.static(path.resolve(__dirname, 'app')));
 
-var renderIndex = (req: express.Request, res: express.Response) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-}
+// var renderIndex = (req: express.Request, res: express.Response) => {
+//     res.sendFile(path.resolve(__dirname, 'index.html'));
+// }
+// 
+// app.get('/*', renderIndex);
 
-app.get('/*', renderIndex);
-
-// var serveStatic = require('serve-static');
-// app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
+var serveStatic = require('serve-static');
+app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
 
 var server = app.listen(port, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('This express app is listening on port:' + port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('This express app is listening on port:' + port);
 });
