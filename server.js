723c6d49ef2f68d5bfdd768cf29be790e0271f1e
Caleb Matthews <calebjmatthews@gmail.com>
2016-10-26 21:26:58 -0500

    Test server.js

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-"use strict";
-const express = require('express');
-const path = require('path');
-var port = process.env.PORT || 8000;
+// import express = require('express');
+// import path = require('path');
+// var port: number = process.env.PORT || 8000;
+// var app = express();
+// app.use('/app', express.static(path.resolve(__dirname, 'app')));
+// // var renderIndex = (req: express.Request, res: express.Response) => {
+// //     res.sendFile(path.resolve(__dirname, 'index.html'));
+// // }
+// // 
+// // app.get('/*', renderIndex);
+// var serveStatic = require('serve-static');
+// app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
+// var server = app.listen(port, function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+//   console.log('This express app is listening on port:' + port);
+// });
+var express = require('express');
 var app = express();
-app.use('/app', express.static(path.resolve(__dirname, 'app')));
-// var renderIndex = (req: express.Request, res: express.Response) => {
-//     res.sendFile(path.resolve(__dirname, 'index.html'));
-// }
-// 
-// app.get('/*', renderIndex);
-var serveStatic = require('serve-static');
-app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
-var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('This express app is listening on port:' + port);
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
 //# sourceMappingURL=server.js.map
