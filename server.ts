0a1254ee39a89bcf4f47dcbcb41eec782cf66bf3
Caleb Matthews <calebjmatthews@gmail.com>
2016-10-26 21:42:01 -0500

    Serve-static add to package.json

--- server.ts
+++ server.ts
@@ -1,35 +1,36 @@
-// import express = require('express');
-// import path = require('path');
-// var port: number = process.env.PORT || 8000;
-// var app = express();
-
-// app.use('/app', express.static(path.resolve(__dirname, 'app')));
-
-// // var renderIndex = (req: express.Request, res: express.Response) => {
-// //     res.sendFile(path.resolve(__dirname, 'index.html'));
-// // }
-// // 
-// // app.get('/*', renderIndex);
-
-// var serveStatic = require('serve-static');
-// app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
-
-// var server = app.listen(port, function() {
-//   var host = server.address().address;
-//   var port = server.address().port;
-//   console.log('This express app is listening on port:' + port);
-// });
-
-var express = require('express');
+import express = require('express');
+import path = require('path');
+var port: number = process.env.PORT || 8000;
 var app = express();
 
-console.log(app);
+app.use('/app', express.static(path.resolve(__dirname, 'app')));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// var renderIndex = (req: express.Request, res: express.Response) => {
+//     res.sendFile(path.resolve(__dirname, 'index.html'));
+// }
+// 
+// app.get('/*', renderIndex);
+
+var serveStatic = require('serve-static');
+app.use(serveStatic(__dirname, { 'index': ['index.html'] }));
+
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('This express app is listening on port:' + port);
 });
 
-var port: number = process.env.PORT || 8000
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port );
-});
+// var express = require('express');
+// var app = express();
+// var port: number = process.env.PORT || 8000
+
+// console.log(app);
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+
+// app.listen(port, function () {
+//   console.log('Example app listening on port ' + port );
+// });
