799b5fe87ce36ac0bf0d50bc8d563f1f7c1736d3
Caleb Matthews <calebjmatthews@gmail.com>
2020-04-26 15:09:15 -0500

    First brush with angled underlying reason

--- client/components/app.tsx
+++ client/components/app.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+const bg = require('../images/capitol.jpg');
+
 export default function App() {
   return (
-    <div>
-      Hello, world!
+    <div className="app">
+      <div className="main">
+        <div className="header">
+          Hello, world!
+        </div>
+        <div className="angled-border angled-border-top"></div>
+        <div className="spacer"></div>
+        <div className="angled-border angled-border-bottom"></div>
+        <div className="body">
+          This is the world, hello.
+        </div>
+      </div>
+      <img className="background-image" src={bg.default} />
     </div>
   )
 }
